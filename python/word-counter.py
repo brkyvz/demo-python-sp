4fd77662da3d1e88f4790a56f1c9ef5486059046
Burak Yavuz <brkyvz@gmail.com>
2015-04-02 13:52:39 -0700

    restructure

--- python/word-counter.py
+++
@@ -1,8 +0,0 @@
-
-class WordCounter():
-    
-    def __init__(self, sc):
-        self.sc = sc
-        
-    def count(self, file):
-        return sc.textFile(file).flatMap(lambda line: line.split()).map(lambda word: (word, 1)).countByKey()
